9c3f237188630bf636ca79a1a0bc7b6ee28bc23b
羊荣毅_L <yangrongyi_em@163.com>
2017-04-09 15:25:12 +0800

    PersonalActivityUI

---
+++ app/src/main/java/com/swpuiot/yikao/view/activity/UserInformationActivity.java
@@ -0,0 +1,45 @@
+package com.swpuiot.yikao.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+
+import com.swpuiot.yikao.R;
+import com.swpuiot.yikao.presenter.userinformation.UserInformationHolder;
+
+public class UserInformationActivity extends AppCompatActivity implements UserInformationHolder.view ,View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_user_information);
+        inite();
+    }
+    private void inite(){
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar_user_information));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        findViewById(R.id.ll).requestFocus();
+        findViewById(R.id.ll_reset_user_logo).setOnClickListener(this);
+        findViewById(R.id.ll_reset_user_name).setOnClickListener(this);
+        findViewById(R.id.ll_reset_user_sex).setOnClickListener(this);
+        findViewById(R.id.ll_reset_user_age).setOnClickListener(this);
+        findViewById(R.id.ll_reset_user_birth).setOnClickListener(this);
+        findViewById(R.id.ll_reset_user_star).setOnClickListener(this);
+        findViewById(R.id.tt_save).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/swpuiot/yikao/view/fragment/PersonalFragment.java
+++ app/src/main/java/com/swpuiot/yikao/view/fragment/PersonalFragment.java
@@ -1,5 +1,6 @@
 package com.swpuiot.yikao.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,22 +12,28 @@ import android.widget.TextView;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.swpuiot.yikao.R;
 import com.swpuiot.yikao.presenter.personalpresent.PerSonalHolder;
+import com.swpuiot.yikao.presenter.personalpresent.PersonalPresent;
+import com.swpuiot.yikao.view.activity.UserInformationActivity;
 
 /**
  * Created by 羊荣毅_L on 2017/3/29.
  * 个人主页
  */
-public class PersonalFragment extends Fragment implements PerSonalHolder.view{
+public class PersonalFragment extends Fragment implements PerSonalHolder.view, View.OnClickListener {
+    private PerSonalHolder.present mPresent;
     private SimpleDraweeView mLogo;
     private TextView mProfession;
     private TextView other;
     private View mView;
+    private Intent intent;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (mView==null){
-            mView=inflater.inflate(R.layout.fragment_personal,container,false);
+        if (mView == null) {
+            mView = inflater.inflate(R.layout.fragment_personal, container, false);
         }
+        inite();
         return mView;
     }
 
@@ -35,9 +42,41 @@ public class PersonalFragment extends Fragment implements PerSonalHolder.view{
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
     }
-    private void inite(){
-        mLogo= (SimpleDraweeView) mView.findViewById(R.id.image_userlogo);
-        mProfession= (TextView) mView.findViewById(R.id.text_user_name);
-        other= (TextView) mView.findViewById(R.id.text_user_other );
+
+    private void inite() {
+        mPresent = new PersonalPresent(getActivity(), this);
+        mLogo = (SimpleDraweeView) mView.findViewById(R.id.image_userlogo);
+        mProfession = (TextView) mView.findViewById(R.id.text_user_name);
+        other = (TextView) mView.findViewById(R.id.text_user_other);
+        mView.findViewById(R.id.ll_personal_user_information).setOnClickListener(this);
+        mView.findViewById(R.id.ll_personal_downloads).setOnClickListener(this);
+        mView.findViewById(R.id.ll_personal_update).setOnClickListener(this);
+        mView.findViewById(R.id.ll_personal_text).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.ll_personal_user_information:
+                intent = new Intent(getContext(), UserInformationActivity.class);
+                mPresent.startActivity(intent);
+                break;
+            case R.id.ll_personal_downloads:
+                // TODO: 2017/4/9
+                break;
+            case R.id.ll_personal_update:
+                // TODO: 2017/4/9
+                break;
+            case R.id.ll_personal_text:
+                // TODO: 2017/4/9
+                break;
+
+        }
+    }
+
+    @Override
+    public void myStartActivity(Intent intent) {
+        startActivity(intent);
     }
 }
